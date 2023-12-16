70938e929bd4481de3889163f8ecf00b92f941e1
udaykiranchenna2 <inspizyprivatelimited@gmail.com>
2023-12-16 23:39:57 +0530

    Input enabled

--- app/page.jsx
+++ app/page.jsx
@@ -106,7 +106,7 @@ export default function Home() {
                 onInput={(e) => {
                   getTextOnInput(e.target.value);
                 }}
-                value="who are you?"
+                defaultValue="who are you?"
                 type="text"
                 id="prompt-input"
                 className="md:w-[75%] w-full  block p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
